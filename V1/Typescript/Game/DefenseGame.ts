0ed6eac2bd5a27056a9701a61c2f0372a1c4bce2
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-06-25 18:10:25 +0200

    moved css to extra folder / file

--- V1/Typescript/Game/DefenseGame.ts
+++ V1/Typescript/Game/DefenseGame.ts
@@ -2,27 +2,41 @@ namespace V1 {
   export class DefenseGame {
     
     private viewport: ƒ.Viewport;
+    private wrapperDiv: HTMLDivElement;
 
-    constructor(canvas: HTMLCanvasElement) {
-      let graph: ƒ.Node = new ƒ.Node("Graph");
+    constructor() {
+      //this.wrapperDiv = _wrapperDiv;
+      // let graph: ƒ.Node = new ƒ.Node("Graph");
 
-      let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
-      cmpCamera.pivot.translate(new ƒ.Vector3(10, 5, 10));
-      cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
-      cmpCamera.backgroundColor = ƒ.Color.CSS("lightblue");
+      // let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+      // cmpCamera.pivot.translate(new ƒ.Vector3(10, 5, 10));
+      // cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
+      // cmpCamera.backgroundColor = ƒ.Color.CSS("lightblue");
 
-      this.viewport = new ƒ.Viewport();
-      this.viewport.initialize("Viewport", graph, cmpCamera, canvas);
-      ƒ.Debug.log(this.viewport);
+      // this.viewport = new ƒ.Viewport();
+      // this.viewport.initialize("Viewport", graph, cmpCamera, canvas);
+      // ƒ.Debug.log(this.viewport);
 
-      ƒAid.addStandardLightComponents(graph, new ƒ.Color(0.5, 0.5, 0.5));
+      // ƒAid.addStandardLightComponents(graph, new ƒ.Color(0.5, 0.5, 0.5));
 
-      ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, this.update.bind(this));
-      ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 30);
+      // ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, this.update.bind(this));
+      // ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 30);
+    }
+
+    public init(): void {
+      this.wrapperDiv.appendChild(new HTMLCanvasElement());
+    }
+
+    public loop(): void {
+      //TODO
     }
 
     private update(_event: ƒ.Eventƒ): void {
       this.viewport.draw();
+    }
+
+    private addCanvas(): void {
+
     }
   }
 }
--- V1/Typescript/Main.ts
+++ V1/Typescript/Main.ts
@@ -1,13 +1,10 @@
 namespace V1 {
-  import ƒAid = FudgeAid;
 
   window.addEventListener("load", hndLoad);
 
   export let viewport: ƒ.Viewport;
 
   function hndLoad(_event: Event): void {
-    let canvas: HTMLCanvasElement = document.querySelector("canvas");
-    let game: DefenseGame = new DefenseGame(canvas);
-    
+    let game: DefenseGame = new DefenseGame();
   }
 }
